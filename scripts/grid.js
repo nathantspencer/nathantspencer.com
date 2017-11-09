a132d97e513b46442c577431f0efdb4ca5205ed1
nathantspencer <nathantspencer@gmail.com>
2017-11-09 07:57:20 -0500

    Resizing updates grid

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,47 +1,70 @@
 (function () {
 
-// Creates and manages the grid in #viewport-box-1
+// GRID.JS
+// Creates and manages the color grid in #viewport-box-1
 
 window.addEventListener("load", createGrid, false);
+window.addEventListener("resize", createGrid);
 
-var grid;
-var baseBoxSizePx = 25;
+var grid = [];
+var boxSize = 25;
+var numberOfBoxesX;
+var numberOfBoxesY;
 
+// Creates a grid of the current window size or expands existing grid
 function createGrid()
 {
 	var root = document.getElementById("viewport-box-1");
 
-	var numberOfBoxesX = Math.floor(window.innerWidth / baseBoxSizePx);
-	var numberOfBoxesY = Math.floor(window.innerHeight / baseBoxSizePx);
-
-	var boxWidth = baseBoxSizePx + (window.innerWidth % baseBoxSizePx) / numberOfBoxesX;
-	var boxHeight = baseBoxSizePx + (window.innerHeight % baseBoxSizePx) / numberOfBoxesY;
-
-	// create an empty 2D array
-	grid = [];
-	for (var i = 0; i < numberOfBoxesX; ++i)
-	{
-		grid[i] = [];
-	}
+	numberOfBoxesX = Math.ceil(window.innerWidth / boxSize);
+	numberOfBoxesY = Math.ceil(window.innerHeight / boxSize);
 
 	for (var i = 0; i < numberOfBoxesX; ++i)
 	{
-		for (var j = 0; j < numberOfBoxesY; ++j)
+		if(i >= grid.length)
+		{
+			grid[i] = []
+		}
+
+		for (var j = grid[i].length; j < numberOfBoxesY; ++j)
 		{
 			grid[i][j] = document.createElement("div");
 			grid[i][j].className = "grid-box"
 
-			grid[i][j].style.width = Math.ceil(boxWidth) + "px";
-			grid[i][j].style.height = Math.ceil(boxHeight) + "px";
-			grid[i][j].style.left = (i * boxWidth) + "px";
-			grid[i][j].style.top = (j * boxHeight) + "px";
+			grid[i][j].style.width = boxSize + "px";
+			grid[i][j].style.height = boxSize + "px";
+			grid[i][j].style.left = (i * boxSize) + "px";
+			grid[i][j].style.top = (j * boxSize) + "px";
+			grid[i][j].style.backgroundColor = grayInRange(230, 237);
 
-			var intensity = 240 + Math.floor(Math.random() * 16);
-			var boxColor = "rgb(" + intensity + ", " + intensity + ", " + intensity + ")"
-			grid[i][j].style.backgroundColor = boxColor;
 			root.appendChild(grid[i][j]);
 		}
 	}
+}
+
+// Takes two colors (rgb array[3]) and returns a random color between
+function colorInRange(minColor, maxColor)
+{
+	rangeR = maxColor[0] - minColor[0];
+	rangeG = maxColor[1] - minColor[1];
+	rangeB = maxColor[2] - minColor[2];
+
+	rgb = [];
+	rgb[0] = minColor[0] + Math.floor(Math.random() * rangeR);
+	rgb[1] = minColor[1] + Math.floor(Math.random() * rangeG);
+	rgb[2] = minColor[2] + Math.floor(Math.random() * rangeB);
+
+	result = "rgb(" + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ")";
+	return result;
+}
+
+// Takes two intensities and returns a grayscale in that rangeB
+function grayInRange(minIntensity, maxIntensity)
+{
+	range = maxIntensity - minIntensity;
+	intensity = minIntensity + Math.floor(Math.random() * range);
+	result = "rgb(" + intensity + ", " + intensity + ", " + intensity + ")";
+	return result;
 }
 
 })();
